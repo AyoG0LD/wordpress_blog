6d62c77372c1dbd109525896215211deaf65c24b
AyoGOLD <Ayoniogrl8@gmail.com>
2023-08-05 22:59:21 -0400

    wp-db

---
+++ wp-db-ip.sh
@@ -0,0 +1,13 @@
+##Update Mysql Database wordpress Ip addressa
+MYSQL_USER="wordpressuser"
+MYSQL_HOST="wordpressdb.cxancv1foguf.us-east-1.rds.amazonaws.com"
+MYSQL_PASS="stackinc"
+MYSQL_DB="wordpressdb"
+log_command PUBLIC_ADDRESS=`echo $(sudo curl -s ipinfo.io/ip)`
+
+SQL_COMMAND="SELECT option_name, option_value FROM wp_options WHERE option_name = 'siteurl';"
+
+OLD_IP=`sudo mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" -D "$MYSQL_DB" -e "$SQL_COMMAND" | awk 'NR==2 {print $2}'`
+
+sudo mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" -D "$MYSQL_DB" -e "UPDATE wp_options SET option_value = '$PUBLIC_ADDRESS' WHERE option_value LIKE '$OLD_IP';"
+
