50f8962f558d21418df9f9237615401ca831d5d1
AyoGOLD <Ayoniogrl8@gmail.com>
2023-08-09 02:33:45 -0400

    clixx-update

--- clixx-db-ip.sh
+++ clixx-db-ip.sh
@@ -3,8 +3,8 @@ MYSQL_USER="wordpressuser"
 MYSQL_HOST="wordpressdbclixx.cxancv1foguf.us-east-1.rds.amazonaws.com"
 MYSQL_PASS="W3lcome123"
 MYSQL_DB="wordpressdb"
-PUBLIC_ADDRESS=`echo $(sudo curl -s ipinfo.io/ip)`
-#LOAD_BALANCER_IP="StackAppLB-941984852.us-east-1.elb.amazonaws.com"
+#PUBLIC_ADDRESS=`echo $(sudo curl -s ipinfo.io/ip)`
+LOAD_BALANCER_IP="Stack-Clixx-LB-1307965364.us-east-1.elb.amazonaws.com"
 SQL_COMMAND="SELECT option_name, option_value FROM wp_options WHERE option_name = 'siteurl';"
 
 OLD_IP=`sudo mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" -D "$MYSQL_DB" -e "$SQL_COMMAND" | awk 'NR==2 {print $2}'`
